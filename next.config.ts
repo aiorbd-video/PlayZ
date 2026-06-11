1168667a587f33c4403b822a04ffdb5cdc1d6602
aiorbd-video <ratulhasan5a@gmail.com>
2026-06-11 12:03:07 +0600

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: '/api/proxy-cats',
+        destination: 'https://ratul-api-all-in-one.onrender.com/event-cats',
+      },
+      {
+        source: '/api/proxy-matches',
+        destination: 'https://ratul-api-all-in-one.onrender.com/get-data/categories/live-events.txt',
+      }
+    ];
+  },
 };
 
 export default nextConfig;
