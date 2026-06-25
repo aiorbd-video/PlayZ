1d330ecab2bf021e0f976e81376937193a79263e
aiorbd-video <ratulhasan5a@gmail.com>
2026-06-25 15:28:01 +0600

    Enhance service worker with Workbox and caching
    Refactor service worker to include Workbox and improve offline handling.

--- public/sw.js
+++ public/sw.js
@@ -1,20 +1,54 @@
-const CACHE_NAME = 'aio-reborn-cache-v1';
+// This is the service worker with the combined offline experience (Offline page + Offline copy of pages)
 
-// ইনস্টল ইভেন্ট
-self.addEventListener('install', (event) => {
-  self.skipWaiting();
+const CACHE = "aio-reborn-cache-v1";
+
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
+
+// TODO: replace the following with the correct offline fallback page i.e.: const offlineFallbackPage = "offline.html";
+const offlineFallbackPage = "ToDo-replace-this-name.html";
+
+self.addEventListener("message", (event) => {
+  if (event.data && event.data.type === "SKIP_WAITING") {
+    self.skipWaiting();
+  }
 });
 
-// অ্যাক্টিভেট ইভেন্ট
-self.addEventListener('activate', (event) => {
-  event.waitUntil(clients.claim());
-});
-
-// ফেচ ইভেন্ট (PWABuilder-এর চেক পাস করার জন্য এটি থাকা বাধ্যতামূলক)
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    fetch(event.request).catch(() => {
-      return new Response('You are offline. Please check your internet connection.Join Telegram channel for app update');
-    })
+self.addEventListener('install', async (event) => {
+  event.waitUntil(
+    caches.open(CACHE)
+      .then((cache) => cache.add(offlineFallbackPage))
   );
+});
+
+if (workbox.navigationPreload.isSupported()) {
+  workbox.navigationPreload.enable();
+}
+
+workbox.routing.registerRoute(
+  new RegExp('/*'),
+  new workbox.strategies.StaleWhileRevalidate({
+    cacheName: CACHE
+  })
+);
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.mode === 'navigate') {
+    event.respondWith((async () => {
+      try {
+        const preloadResp = await event.preloadResponse;
+
+        if (preloadResp) {
+          return preloadResp;
+        }
+
+        const networkResp = await fetch(event.request);
+        return networkResp;
+      } catch (error) {
+
+        const cache = await caches.open(CACHE);
+        const cachedResp = await cache.match(offlineFallbackPage);
+        return cachedResp;
+      }
+    })());
+  }
 });
