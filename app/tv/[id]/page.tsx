14a57e996994406a2fc68fe80dd05f935c440414
aiorbd-video <ratulhasan5a@gmail.com>
2026-06-25 21:37:14 +0600

    Update Page component to accept params
    Pass parameters to ClientPage component from Page function.

--- app/tv/[id]/page.tsx
+++ app/tv/[id]/page.tsx
@@ -5,11 +5,11 @@ export async function generateStaticParams() {
   return [{ id: '1' }]; 
 }
 
-// এখানে কোনো params পাঠানো হচ্ছে না, কারণ ClientPage নিজে থেকেই তা ম্যানেজ করে নেবে
-export default function Page() {
+export default function Page({ params }: any) {
   return (
     <Suspense fallback={<div className="min-h-screen bg-[#11131A]"></div>}>
-      <ClientPage />
+      {/* @ts-ignore - টাইপস্ক্রিপ্টের পণ্ডিতি বন্ধ করার ম্যাজিক ট্রিক */}
+      <ClientPage params={params} />
     </Suspense>
   );
 }
