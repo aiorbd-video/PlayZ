97cf00f98375f65d557cc07475a4abf90c29fb58
aiorbd-video <ratulhasan5a@gmail.com>
2026-06-25 20:47:14 +0600

    Update layout.tsx

--- app/tv/[id]/layout.tsx
+++ app/tv/[id]/layout.tsx
@@ -1,12 +1,7 @@
-// এটি সার্ভার কম্পোনেন্ট, তাই এখানে এটি ১০০% কাজ করবে এবং বিল্ড এরর বাইপাস করবে
 export function generateStaticParams() {
   return [];
 }
 
-export default function PlaylistLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function TvLayout({ children }: { children: React.ReactNode }) {
   return <>{children}</>;
 }
