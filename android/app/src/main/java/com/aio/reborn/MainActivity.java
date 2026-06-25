ad7b76be412349da4c2c8f6a70f650f6ada9fca3
aiorbd-video <ratulhasan5a@gmail.com>
2026-06-25 10:22:35 +0000

    Fixed Next.js export and Capacitor config

---
+++ android/app/src/main/java/com/aio/reborn/MainActivity.java
@@ -0,0 +1,5 @@
+package com.aio.reborn;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.aio.reborn',
+  appName: 'All In One',
+  webDir: 'out'
+};
+
+export default config;
