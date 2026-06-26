bfe7995e5a1a335df332884f3bc5ec5da237e847
aiorbd-video <ratulhasan5a@gmail.com>
2026-06-26 08:14:49 +0600

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,20 @@
-import type { CapacitorConfig } from '@capacitor/cli';
+import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.aio.reborn',
-  appName: 'All In One',
-  webDir: 'out'
+  appId: 'com.playz.app', // আপনার অ্যাপের আইডি যেমন আছে তেমনই রাখবেন
+  appName: 'PlayZ', // আপনার অ্যাপের নাম
+  webDir: 'out',
+  bundledWebRuntime: false,
+  server: {
+    androidScheme: 'https',
+    cleartext: true
+  },
+  // 🎯 এই প্লাগিন অংশটুকু যোগ করুন (এটাই CORS বাইপাস করবে)
+  plugins: {
+    CapacitorHttp: {
+      enabled: true,
+    },
+  },
 };
 
 export default config;
